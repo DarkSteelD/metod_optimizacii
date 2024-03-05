eb22cf94ae14f16bf1874a6975f324c2d8257469
Далер <101361201+DarkSteelD@users.noreply.github.com>
2024-03-05 11:20:50 +0300

    Update Dixotomia.py

--- Dixotomia.py
+++ Dixotomia.py
@@ -1,38 +1,45 @@
 import numpy as np
 import matplotlib.pyplot as plt
-def dichotomy(f, a, b, tol=1e-5):
+
+def dichotomy(f, a, b, tol=1e-3, delta=1e-5):
     x_p = []
     y_p = []
     x = np.linspace(a, b, 400)    
-    plt.figure(figsize=(b-a, 6)) 
+    plt.figure(figsize=(8, 6))
     y = f(x)
     plt.plot(x, y, label='f(x)', color='red',zorder=-1)
     while (b - a) > tol:
-        x1 = a + (b - a) / 3
-        x2 = b - (b - a) / 3
+        print(a)
+        print(b)
+        midpoint = (a + b)
+        x1 = (midpoint - delta)/2
+        x2 = (midpoint + delta)/2
         f_x1 = f(x1)
         f_x2 = f(x2)
-        
+        print(x1)
+        print(x2)
+        print(str(f_x1) + " " + str(f_x2))
         x_p.extend([x1, x2])
         y_p.extend([f_x1, f_x2])
         
-        if f_x1 > f_x2:
-            a = x1
-        else:
+        if f_x1 < f_x2:
             b = x2
-        
+        else:
+            a = x1
     min_p = (a + b) / 2
-    plt.scatter(x_p, y_p, color='black', label='Точки Сходимости')
-    plt.scatter(min_p, f(min_p), color='gray', s=100, label='Точка Минимума', zorder=2)
+    plt.scatter(x_p, y_p, color='black', label='Точки сходимости')
+    plt.scatter(min_p, f(min_p), color='blue', s=100, label='Точка минимума', zorder=2)
     plt.xlabel('x')
     plt.ylabel('f(x)')
-    plt.title('Метод Дихотомии')
+    plt.title('Метод дихотомии')
     plt.legend()
     plt.grid(True)
     plt.show()
     
     return min_p
+
 def f(x):
-    return (x)**2 + 4*x +6
+    return x**2 + 4*x + 6
+
 min_p = dichotomy(f, -4, 6)
-print(f"Минимум функции находится в точке: {min_p:.5f}, значение функции в этой точке {f(min_p)}")
+print(f"Минимум функции находится в точке: {min_p:.5f}, значение функции в этой точке: {f(min_p):.5f}")
